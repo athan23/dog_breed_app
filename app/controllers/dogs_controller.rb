627b0c360f60f0bcf3d1810fb0ac78d937cb6273
Your Name <you@example.com>
2024-05-17 01:28:19 +0700

    Add fetching controller and view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,24 @@
 class DogsController < ApplicationController
   def index
   end
+
+  def fetch_breed_image
+    breed = params[:breed]
+    result = DogApi.call(breed)
+
+    if result["status"] == "success"
+      @image_url = result["message"]
+      @breed = breed
+    else
+      @image_url = nil
+      @breed = breed
+    end
+
+    puts @image_url
+    puts @breed
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "dogs#index"
+  post 'dogs/fetch_breed_image', to: 'dogs#fetch_breed_image'
 end
